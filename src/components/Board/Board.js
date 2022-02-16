1a0b16919e4d680082674af76710aff0a9eaf60f
Sean King <seanianking@gmail.com>
2022-02-16 13:18:38 -0700

    revisions

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -8,9 +8,7 @@ const Board = (props) => {
     gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
   }
-  const tiles = props.tiles.map((tile) => {
-    <Tile {...tile}/>
-  })
+  const tiles = props.tiles.map((tile) => (<Tile {...tile}/>))
 
   return (
     <div className='Board' style={gridConfig}>
