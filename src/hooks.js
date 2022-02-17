c031e98054181b43a7b2750fd0ae7759741e24a9
Sean King <seanianking@gmail.com>
2022-02-17 15:08:19 -0700

    revisions

--- src/hooks.js
+++ src/hooks.js
@@ -1,21 +1,21 @@
-import { useState, useRef, useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react'
 const useHover = () => {
     const ref = useRef()
-    const [hovered, setHovered] = useState(false);
+    const [hovered, setHovered] = useState(false)
 
     const enter = () => setHovered(true)
     const leave = () => setHovered(false)
 
     useEffect(() => {
-        const refCopy = ref;
-        refCopy.current.addEventListener("mouseenter", () => { enter() })
-        refCopy.current.addEventListener("mouseleave", () => { leave() })
+        const refCopy = ref
+        refCopy.current.addEventListener('mouseenter', enter)
+        refCopy.current.addEventListener('mouseleave', leave)
         return () => {
-            refCopy.current.removeEventListener("mouseenter", () => { enter() })
-            refCopy.current.removeEventListener("mouseleave", () => { leave() })
+            refCopy.current.removeEventListener('mouseenter', enter)
+            refCopy.current.removeEventListener('mouseleave', leave)
         }
     })
     return [ref, hovered]
 }
 
-export default useHover;
+export default useHover
