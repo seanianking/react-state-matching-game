1ffd489cdaea9dc801f320e30ccf932319d0c2ad
Sean King <seanianking@gmail.com>
2022-02-17 15:00:42 -0700

    revisions

--- src/hooks.js
+++ src/hooks.js
@@ -3,20 +3,21 @@ import { useState, useRef, useEffect } from 'react';
 const useHover = () => {
     const ref = useRef()
     const [hovered, setHovered] = useState(false);
-    const enter = () => {
-        setHovered(true)
-    }
-    const leave = () => {
-        setHovered(false)
-    }
+
+    const enter = () => setHovered(true)
+
+    const leave = () => setHovered(false)
+
     useEffect(() => {
         const refCopy = ref;
         //listeners for entering/leaving the tile
         refCopy.current.addEventListener("mouseenter", () => { enter() })
         refCopy.current.addEventListener("mouseleave", () => { leave() })
         //remove event listeners
-        refCopy.current.removeEventListener("mouseenter", () => { enter() })
-        refCopy.current.removeEventListener("mouseleave", () => { leave() })
+        return () => {
+            refCopy.current.removeEventListener("mouseenter", () => { enter() })
+            refCopy.current.removeEventListener("mouseleave", () => { leave() })
+        }
 
     })
     return [ref, hovered]
